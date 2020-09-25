b1e657f6337250860d1337c834f95528e40fe0ac
Sarthak Musmade <sarthakmusmade@gmail.com>
2020-09-25 21:35:51 +0530

    small commit

--- app/src/main/java/com/example/sppucomptextbooks/OOPScreen.java
+++ app/src/main/java/com/example/sppucomptextbooks/OOPScreen.java
@@ -1,8 +1,12 @@
 package com.example.sppucomptextbooks;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +19,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+import static android.os.Environment.getDownloadCacheDirectory;
+import static android.os.Environment.getExternalStorageState;
+
 public class OOPScreen extends AppCompatActivity {
 
     Button downloadBtn;
@@ -22,10 +32,14 @@ public class OOPScreen extends AppCompatActivity {
     StorageReference mStorageRef;
     StorageReference storageRef;
 
+    //Todo : Users must not get the pdf .......
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oop);
+
+
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
@@ -38,6 +52,7 @@ public class OOPScreen extends AppCompatActivity {
             }
         });
 
+
     }
 
     public void download(){
@@ -48,10 +63,16 @@ public class OOPScreen extends AppCompatActivity {
             @Override
             public void onSuccess(Uri uri) {
                 String url = uri.toString();
-                Intent intent = new Intent();
-                intent.setDataAndType(Uri.parse(url), "application/pdf");
-                Intent chooserIntent = Intent.createChooser(intent, "Open Report");
-                startActivity(chooserIntent);
+                downloadFile(OOPScreen.this, "syllabus", "pdf",DIRECTORY_DOWNLOADS, url);
+
+//                Intent intent = new Intent(OOPScreen.this, PDFViewer.class);
+//                intent.putExtra("pdfURL", url);
+//                startActivity(intent);
+//                Intent intent = new Intent();
+//                intent.setDataAndType(Uri.parse(url), "application/pdf");
+//                Intent chooserIntent = Intent.createChooser(intent, "Open Report");
+//                startActivity(chooserIntent);
+
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -61,16 +82,17 @@ public class OOPScreen extends AppCompatActivity {
         });
     }
 
-//    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
-//
-//        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-//        Uri uri = Uri.parse(url);
-//        DownloadManager.Request request = new DownloadManager.Request(uri);
-//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-//        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
-//        downloadManager.enqueue(request);
-//
-//    }
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
+
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
+        downloadManager.enqueue(request);
+
+    }
 
 
     @Override
--- app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
+++ app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
@@ -1,43 +1,83 @@
 package com.example.sppucomptextbooks;
 
-import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+import com.shockwave.pdfium.PdfDocument;
 
-import java.io.FileOutputStream;
-
-import static android.os.Environment.DIRECTORY_DOWNLOADS;
-
-public class PDFViewer extends AppCompatActivity {
-
+import java.util.List;
 
 
+public class PDFViewer extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener, OnPageErrorListener {
 
+
+
+    int pageNumber = 0;
+    PDFView pdfView;
+    String pdfFileName = "syllabus.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_viewer);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);//To PREVENT SCREENSHOT
 
+        pdfView = findViewById(R.id.pdfView);
+
+        String pdfURL = getIntent().getStringExtra("pdfURL");
+
+
+        pdfView.fromUri(Uri.parse(pdfURL))
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .spacing(10) // in dp
+                .onPageError(this)
+                .pageFitPolicy(FitPolicy.BOTH)
+                .load();
     }
 
     @Override
-    public void onBackPressed() {
-//        finish();
-        Intent a = new Intent(PDFViewer.this, OOPScreen.class);
-        startActivity(a);
+    public void loadComplete(int nbPages) {
+
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+
+
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+    @Override
+    public void onPageError(int page, Throwable t) {
+        Log.e("TAG", "Cannot load page " + page);
+    }
+
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e("TAG", String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
     }
 }
