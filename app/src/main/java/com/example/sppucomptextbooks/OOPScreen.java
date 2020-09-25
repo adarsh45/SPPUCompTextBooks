97f695f6306de3582bdeca431fdb1217d41a7a3b
Sarthak Musmade <sarthakmusmade@gmail.com>
2020-09-25 20:26:58 +0530

    Added PDFViewer

--- app/src/main/java/com/example/sppucomptextbooks/OOPScreen.java
+++ app/src/main/java/com/example/sppucomptextbooks/OOPScreen.java
@@ -1,35 +1,76 @@
 package com.example.sppucomptextbooks;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class OOPScreen extends AppCompatActivity {
 
-    // OOP Folder screen - Will contain pdf ....
+    Button downloadBtn;
 
-    Button sampleBtn;
+    StorageReference mStorageRef;
+    StorageReference storageRef;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oop);
 
-        sampleBtn = findViewById(R.id.sampleBtn);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        sampleBtn.setOnClickListener(new View.OnClickListener() {
+        downloadBtn = findViewById(R.id.downloadBtn);
+
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(OOPScreen.this, PDFViewer.class);
-                startActivity(intent);
+                download();
             }
         });
 
     }
+
+    public void download(){
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        storageRef = mStorageRef.child("syllabus.pdf");
+
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url = uri.toString();
+                Intent intent = new Intent();
+                intent.setDataAndType(Uri.parse(url), "application/pdf");
+                Intent chooserIntent = Intent.createChooser(intent, "Open Report");
+                startActivity(chooserIntent);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+//    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
+//
+//        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+//        Uri uri = Uri.parse(url);
+//        DownloadManager.Request request = new DownloadManager.Request(uri);
+//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+//        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
+//        downloadManager.enqueue(request);
+//
+//    }
 
 
     @Override
--- app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
+++ app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,62 +17,20 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.FileOutputStream;
+
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class PDFViewer extends AppCompatActivity {
 
-    // TODO : Setup Pdf reader
-    // TODO: like LCO downloadable but not transferable
 
-    Button downloadBtn;
 
-    StorageReference mStorageRef;
-    StorageReference storageRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_viewer);
-
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-
-        downloadBtn = findViewById(R.id.downloadBtn);
-
-        downloadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                download();
-            }
-        });
-
-    }
-
-    public void download(){
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-        storageRef = mStorageRef.child("syllabus.pdf");
-
-        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                String url = uri.toString();
-                downloadFile(PDFViewer.this, "syllabus", "pdf", DIRECTORY_DOWNLOADS, url);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
-        });
-    }
-
-    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
-
-        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(url);
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
-        downloadManager.enqueue(request);
 
     }
 
