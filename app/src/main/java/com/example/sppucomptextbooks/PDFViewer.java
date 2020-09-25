d744a65dbe3efa9a13b240312c66fc301ffaa861
Sarthak Musmade <sarthakmusmade@gmail.com>
2020-09-25 17:22:02 +0530

    New Commit

--- app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
+++ app/src/main/java/com/example/sppucomptextbooks/PDFViewer.java
@@ -1,22 +1,77 @@
 package com.example.sppucomptextbooks;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class PDFViewer extends AppCompatActivity {
 
     // TODO : Setup Pdf reader
     // TODO: like LCO downloadable but not transferable
 
+    Button downloadBtn;
+
+    StorageReference mStorageRef;
+    StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_viewer);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
+        downloadBtn = findViewById(R.id.downloadBtn);
+
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                download();
+            }
+        });
+
+    }
+
+    public void download(){
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        storageRef = mStorageRef.child("syllabus.pdf");
+
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url = uri.toString();
+                downloadFile(PDFViewer.this, "syllabus", "pdf", DIRECTORY_DOWNLOADS, url);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
+
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
+        downloadManager.enqueue(request);
 
     }
 
