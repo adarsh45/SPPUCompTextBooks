4c05991b2a3ce3e5af7662fd6ef9b07261082fd0
Adarsh Shete <shete.adarsh@gmail.com>
2020-10-07 16:29:23 +0530

    added payment feature using UPI

--- app/src/main/java/com/example/sppucomptextbooks/FoldersScreen.java
+++ app/src/main/java/com/example/sppucomptextbooks/FoldersScreen.java
@@ -16,14 +16,23 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.shreyaspatil.easyupipayment.EasyUpiPayment;
+import com.shreyaspatil.easyupipayment.exception.AppNotFoundException;
+import com.shreyaspatil.easyupipayment.listener.PaymentStatusListener;
+import com.shreyaspatil.easyupipayment.model.TransactionDetails;
 
-public class FoldersScreen extends AppCompatActivity {
+public class FoldersScreen extends AppCompatActivity implements PaymentStatusListener {
+
+    public static final String TAG = "Payment button";
 
     // Folders Screen - Buttons to move to Specific Subject
 
     TextView textSubject, textFDS, textDM, textOOP, textDELD, textCG;
 
     Button fdsBtn1, dmBtn1, oopBtn1, deldBtn1, cgBtn1;
+
+    private TextView textPayStatus;
+
 
 //TODO : PDF test to be done with intent
 
@@ -32,6 +41,7 @@ public class FoldersScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folder);
 
+        textPayStatus = findViewById(R.id.text_pay_status);
 
         FirebaseAuth.getInstance();
 
@@ -107,11 +117,53 @@ public class FoldersScreen extends AppCompatActivity {
     }
 
 
+    public void payThroughUPI(View view){
+        EasyUpiPayment.Builder builder = null;
+        EasyUpiPayment easyUpiPayment = null;
+        try {
+            builder = new EasyUpiPayment.Builder(FoldersScreen.this)
+                    .setPayeeVpa("8669297840@ybl")
+                    .setPayeeName("Atharva Dhoot")
+                    .setTransactionId("TCOMPSEB1406")
+                    .setTransactionRefId("T123456789")
+                    .setDescription("Test for UPI payment app")
+                    .setAmount("5.00");
+        } catch (IllegalStateException isExc){
+            Log.d(TAG, "payThroughUPI: "+ isExc.getLocalizedMessage());
+            Toast.makeText(this, isExc.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                easyUpiPayment = builder.build();
+            } catch (AppNotFoundException e){
+                Log.d(TAG, "payThroughUPI: "+ e.getLocalizedMessage());
+                Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                easyUpiPayment.startPayment();
+                easyUpiPayment.setPaymentStatusListener(this);
+            }
+        }
+
+    }
+
+
+
+
     @Override
     public void onBackPressed() {
         Intent a = new Intent(Intent.ACTION_MAIN);
         a.addCategory(Intent.CATEGORY_HOME);
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
+    }
+
+    @Override
+    public void onTransactionCancelled() {
+        textPayStatus.setText("Transaction cancelled!");
+    }
+
+    @Override
+    public void onTransactionCompleted(TransactionDetails transactionDetails) {
+        Log.d(TAG, "onTransactionCompleted: "+ transactionDetails.toString());
+        textPayStatus.setText(transactionDetails.toString());
     }
 }
